4b8c3b36ff47cf41fcd4030c3d09ba4bb22473b1
sam <sam@192.168.100.123>
2017-04-02 16:49:07 -0400

    Initial commit

---
+++ src/main/java/com/intellimed/spring_demo/App.java
@@ -0,0 +1,21 @@
+package com.intellimed.spring_demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class App {
+
+	public static void main(String[] args){
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		//Triangle triangle=(Triangle) factory.getBean("triangle");
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		
+		triangle.draw();
+		
+	}
+}
---
+++ src/main/java/com/intellimed/spring_demo/Triangle.java
@@ -0,0 +1,19 @@
+package com.intellimed.spring_demo;
+
+public class Triangle {
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	public void draw(){
+		System.out.println(getType() + " triangle is drawn!");
+	}
+
+}
